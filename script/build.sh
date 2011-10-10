62b09a4cf696bb15b0f50f5b58dc49b0dc9a7c6b
segg <wzg312@msn.com>
2011-10-09 20:05:59 -0400

    first commit

---
+++ script/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+menu=../layout/menu.html
+echo "<ul class='dropdown'><li><a href='#'>Albums</a><ul class='sub_menu'>" > $menu
+find . -type d|sort -n -r|while read directory
+do
+	if [ "$directory" = "." ];
+	then
+		directory=Home
+	fi
+	echo $directory
+	directory=${directory/.*\//}
+	echo "<li><a href='#' onclick='replaceContent(\"${directory/'/\\\'/}\")'>$directory</a></li>" >> $menu
+done
+echo "</ul></li></ul>" >> $menu
+
+
+find . -type d|while read directory
+do
+	if [ "$directory" = "." ];
+	then
+		path=.
+		name=Home
+		file=../content/Home.html
+	else
+		path=${directory/.*\//}
+		name=$path
+		file=../content/$name.html
+	fi
+	echo $file
+	rm "$file"
+	ls "$directory"|grep -v thumb|grep jpg -i|while read line
+	do
+		echo "<a class='photo' rel='p' href=\"photos/$path/$line\" ><img src=\"photos/$path/$line.thumb.jpg\" onmouseover=\"javascript:this.id='caller';relocate();\" onmouseout=\"javascript:this.id='';hide();\" /></a>" >> "$file"
+	done
+	echo '<img height=300 id="src" style="position:absolute;display:none;"></script><SCRIPT LANGUAGE="javascript">src=$("#src");function relocate(){caller=$("#caller");url=caller.attr("src");src.attr("src",url.substring(0,url.indexOf(".thumb.jpg")));var offset=caller.offset();if(offset.left<($(document).scrollLeft()+$(window).width()-500)){x=offset.left+103;}else{x=offset.left-400;}y=offset.top;if(offset.top>$(document).scrollTop()+$(window).height()-320){y=$(document).scrollTop()+$(window).height()-320;}src.css("left",x);src.css("top",y);src.css("border","10px solid white");src.fadeIn();}function hide(){src.hide();}$("a.photo").fancybox();</SCRIPT>' >> "$file"
+done
---
+++ script/resize.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+ls *.jpg *.JPG */*.jpg */*.JPG|grep -v thumb|while read file
+do
+	echo $file
+	thumbfile=$file.thumb.jpg
+	height=`identify "$file"|sed 's/.*JPEG //'|cut -d" " -f1|cut -d"x" -f2`
+	if [ $height -gt 800 ];
+	then
+		convert "$file" -verbose -resize 800x800  "$file"
+	fi
+	if [ -f "$thumbfile" ];
+	then
+		height=`identify "$thumbfile"|sed 's/.*JPEG //'|cut -d" " -f1|cut -d"x" -f2`
+		if [ $height -ne 90 ];
+		then
+			convert "$file" -verbose -resize 200x90 "$thumbfile"
+		fi
+	else
+		convert "$file" -verbose -resize 200x90 "$thumbfile"
+	fi
+#	if [ -f "$thumbfile" ];
+#	then
+#		height=`identify "$thumbfile"|sed 's/.*JPEG //'|cut -d" " -f1|cut -d"x" -f2`
+#		if [ $height -gt 72 ];
+#		then
+#			width=`identify "$thumbfile"|sed 's/.*JPEG //'|cut -d" " -f1|cut -d"x" -f1`
+#			diff=`expr $height - 72`
+#			diff=`expr $diff / 2`
+#			convert "$thumbfile" -shave 0x$diff  "$thumbfile"
+#		fi
+#	fi
+done
+
+
